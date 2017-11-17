8002823a8c3335fd6012dfb860b1c94125718d52
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-17 21:49:47 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -3,14 +3,14 @@ class CouponsController < ApplicationController
   def index
     @coupons = Coupon.all
   end
-
-  def create_table
-    #the new form should be hooked into this and create a new instance of table and i would guess redirect to show
-  end
-
-  def new
-  end
   
+      def new
+      end
+  
+      def create_table
+        #the new form should be hooked into this and create a new instance of table and i would guess redirect to show
+      end
+
   def show
   end
 end
