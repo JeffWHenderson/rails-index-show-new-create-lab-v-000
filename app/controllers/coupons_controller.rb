032fa76170e0cd0eea80c16116504fb22f6a4313
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-17 22:05:36 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -8,7 +8,6 @@ class CouponsController < ApplicationController
       end
 
       def create
-        raise params.inspect
         @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
         redirect_to coupon_path(@coupon)
       end
